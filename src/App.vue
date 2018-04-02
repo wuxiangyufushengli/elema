1759996372b4d3c471e8d974ade464f6d4bf251b
xiangyu <18910256536@163.com>
2018-04-02 22:01:55 +0800

    ajax

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,15 @@
 
 </template>
 <script>
+   import {reqAddress} from './api/index'
     import FooterGuide from './components/FooterGuide/FooterGuide'
     export default {
-    components:{FooterGuide}
+    components:{FooterGuide},
+      async mounted () {
+        const result = await reqAddress('40.10038,116.37867')
+        console.log(result)
+      },
+
     }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
