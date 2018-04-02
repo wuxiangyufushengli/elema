de6c7a53261aa7f3685ec4f8ee3a460819ee91e0
xiangyu <18910256536@163.com>
2018-04-02 18:26:04 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <footer-guide></footer-guide>
+  </div>
+
+</template>
+<script>
+    import FooterGuide from './components/FooterGuide/FooterGuide'
+    export default {
+    components:{FooterGuide}
+    }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router
+})
